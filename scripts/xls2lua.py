bc74f40f1ba2b3c5670e7d2c931f2e33c17b6c12
pei.luo <pei.luo@funplus.com>
2022-06-10 04:47:01 +0000

    add lua and json output

--- scripts/xls2lua.py
+++ scripts/xls2lua.py
@@ -16,87 +16,54 @@ sys.path.append("..")
 from google.protobuf.descriptor import FieldDescriptor
 
 datemode = 0 # 时间戳模式 0: 1900-based, 1: 1904-based
+BOOL_YES = ["yes", "1", "是"]
+BOOL_NO = ["", "nil", "0", "false", "no", "none", "否", "无"]
 
 class Converter:
     _config = {}
     _xls2class = {}
-    _indent = u"    "
+    _indent = "    "
     def __init__(self, config):
         self._config = config
         for line in open(self._config.maps):
             items = line.split('=')
             filename = items[0].strip().encode("utf-8").decode('utf-8')
-            classname = items[1].strip(u" \n").encode("utf-8").decode('utf-8')
+            classname = items[1].strip(" \n").encode("utf-8").decode('utf-8')
             self._xls2class[filename] = classname
 
-    def _objectToStringWithIndent(self, data, level=1, func=None):
-        if isinstance(data, list) or isinstance(data, tuple):
-            content = []
-            for item in data:
-                content.append(self._objectToStringWithIndent(item, level + 1, func))
-            content = self._indent*(level+1) + (u",\n"+self._indent*(level+1)).join(content)
-            result = u"{\n%s\n%s}" % (content, self._indent*(level))
+    def _toLua(self, data, level=1):
+        lines = []
+        if isinstance(data, list):
+            items = []
+            for value in data:
+                value = self._toLua(value, level+1)
+                items.append("%s%s"%(self._indent*level, value))
+            lines.append("{\n%s\n%s}"%(", \n".join(items), self._indent*(level-1)))
         elif isinstance(data, dict):
-            content = []
-            for key in sorted(data.keys(), func):
-                value = data[key]
-                if isinstance(key, str):
-                    content.append(u"%s = %s" % (key.strip('"'), self._objectToStringWithIndent(value, level+1, func)))
+            items = []
+            for (key, value) in data.items():
+                if isinstance(key, int):
+                    key = "[%d]"%(key)
+                elif isinstance(key, str):
+                    pass
                 else:
-                    content.append(u"[%s] = %s" % (key, self._objectToStringWithIndent(value, level+1, func)))
-            content = self._indent*(level+1) + (u",\n"+self._indent*(level+1)).join(content)
-            result = u"{\n%s\n%s}" % (content, self._indent*level)
+                    raise Exception("Error: {}({}) can't be a key.".format(key, type(key)))
+                value = self._toLua(value, level+1)
+                items.append("{}{} = {}".format(self._indent*level, key, value))
+            lines.append("{\n%s\n%s}"%(", \n".join(items), self._indent*(level-1)))
+        elif isinstance(data, str):
+            return '[[%s]]'%(data)
+        elif isinstance(data, bool):
+            return 'true' if data else 'false'
         else:
-            result = u"%s" % data
-        return result
+            return str(data)
+        return ", \n".join(lines)
 
-    def _objectToString(self, data, func=None):
-        if isinstance(data, list) or isinstance(data, tuple):
-            content = []
-            for item in data:
-                content.append(self._objectToString(item, func))
-            content = u",".join(content)
-            result = u"{%s}" % (content)
-        elif isinstance(data, dict):
-            content = []
-            for key in sorted(data.keys(), key=func):
-                value = data[key]
-                content.append(u"%s=%s" % (key, self._objectToString(value, func)))
-            content = u",".join(content)
-            result = u"{%s}" % (content)
-        else:
-            result = u"%s" % data
-        return result
-
-    def getCode(self, data, mainkey, field_sort_func=None):
-        rows = []
-        tostring = self._objectToString
-        for row in data:
-            print("============", data)
-            if not mainkey:
-                rows.append(u"%s%s" % (self._indent, tostring(row, func=field_sort_func)))
-            else:
-                key = row[mainkey]
-                del row[mainkey]
-                left_split = u"["
-                right_split = u"]"
-                if isinstance(key, str):
-                    left_split = u""
-                    right_split = u""
-                    key = key.strip(u'"')
-                if len(row.items()) == 1:
-                    print("============", data)
-                    rows.append(u"%s%s%s%s = %s" % (self._indent, left_split, key,  right_split, tostring(row.items()[0][1], func=field_sort_func)))
-                else:
-                    rows.append(u"%s%s%s%s = %s" % (self._indent, left_split, key, right_split, tostring(row, func=field_sort_func)))
-        code = u"{\n%s\n}" % (u",\n".join(rows))
-        return code
-
-    def save(self, filename, data):
-        lua_dir = self._config.output_dir
-        if not os.path.exists(lua_dir):
-            os.makedirs(lua_dir)
-        filepath = u"%s/%s"% (lua_dir, filename)
+    def save(self, output_type, filename, data):
+        out_dir = "{}/{}".format(self._config.output_dir, output_type)
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        filepath = "{}/{}.{}".format(out_dir, filename, output_type)
         with open(filepath, 'wb') as f:
             f.write(data.encode('utf-8'))
 
@@ -106,10 +73,10 @@ class Converter:
 
     def convert(self, filename):
         if filename not in self._xls2class:
-            raise Exception(u"Failed to load config of this file. %s" % filename)
+            raise Exception("Failed to load config of this file. %s" % filename)
         classname = self._xls2class[filename]
 
-        filepath = u"%s/%s" % (self._config.input_dir, filename)
+        filepath = "%s/%s" % (self._config.input_dir, filename)
         worksheet = xlrd.open_workbook(filepath)
         sheet = worksheet.sheet_by_index(0)
         nrows = sheet.nrows
@@ -131,9 +98,11 @@ class Converter:
                 assert(mainkey == None)
                 mainkey = name
             field2index[col] = {"desc":desc, "name":name, "type":vtype, "levels":name.split('#')}
-        #print(json.dumps(field2index, indent=4))
 
-        result = []
+        if mainkey is None:
+            result = []
+        else:
+            result = {}
         for row in range(3, nrows):
             item = {"_meta":{"isdict":True}}
             fields = {}
@@ -148,35 +117,25 @@ class Converter:
                 else:
                     fields[key] = value
             self._convertRow(item, fields)
-            #print(json.dumps(item, indent=4))
             item = self._fixLevelType(item)
-            #print(json.dumps(item, indent=4))
-            result.append(item)
+            if isinstance(result, list):
+                result.append(item)
+            else:
+                k = item[mainkey]
+                result[k] = item
 
-        #print(result)
-        print(json.dumps(result, indent=4))
+        #print(json.dumps(result, indent=4))
 
-        #def field_sort_func(x, y):
-        #    if x in field2index and y in field2index:
-        #        return field2index[x] - field2index[y]
-        #    elif x in field2index and y not in field2index:
-        #        return -1
-        #    elif x not in field2index and y in field2index:
-        #        return 1
-        #    else:
-        #        return x < y
-
-        #def field_sort_func(x):
-        #    if x in field2index:
-        #        return field2index[x]
-        #    else:
-        #        return 0
-        #code = self.getCode(result, mainkey, field_sort_func)
-        #code = u"_G.tables = _G.tables or {}\n_G.tables.%s = %s" % (classname, code)
-        #print(code)
+        if self._config.type == "lua":
+            luaTable = self._toLua(result)
+            code = "return %s" % (luaTable)
+        elif self._config.type == "json":
+            code = json.dumps(result, indent=4)
+        else:
+            raise Exception("Error: invalid output type.")
 
         # save
-        #self.save(classname + '.lua', code)
+        self.save(self._config.type, classname, code)
 
     def _convertRow(self, result, fields):
         if len(fields) == 0 :
@@ -223,7 +182,10 @@ class Converter:
         if meta["isdict"]:
             pass
         else:
-            item = list(item.values()) # todo 检查values返回的顺序
+            tmp = []
+            for k in sorted(item.keys()):
+                tmp.append(item[k])
+            item = tmp
         return item
 
     def _getCellValue(self, cell, vtype):
@@ -236,54 +198,75 @@ class Converter:
         elif vtype == "string": # string
             return self._getCellString(cell)
         else:
-            raise Exception(u"This type is invalid. %s" % vtype)
+            raise Exception("This type is invalid. %s" % vtype)
         
     def _getCellString(self, cell):
-        cell_text = ""
-        if cell.ctype == xlrd.XL_CELL_TEXT:
-            cell_text = cell.value
+        if cell.ctype == xlrd.XL_CELL_EMPTY:
+            return ""
+        elif cell.ctype == xlrd.XL_CELL_TEXT:
+            return cell.value
         elif cell.ctype == xlrd.XL_CELL_NUMBER:
-            cell_text = (u"%.2f" % cell.value).rstrip('0').rstrip('.')
+            return ("%.2f" % cell.value).rstrip('0').rstrip('.')
         elif cell.ctype == xlrd.XL_CELL_DATE:
             dt = xlrd.xldate.xldate_as_datetime(cell.value, datemode)
-            cell_text = u"%s" % dt
+            return "%s" % dt
         elif cell.ctype == xlrd.XL_CELL_BOOLEAN:
-            cell_text =  u"true" if cell.value else u"false"
-        return cell_text
+            return "true" if cell.value else "false"
+        else:
+            raise Exception("Error: invalid cell type. type:{}".format(cell.ctype))
     
     def _getCellInt(self, cell):
-        if cell.ctype == xlrd.XL_CELL_TEXT or cell.ctype == xlrd.XL_CELL_NUMBER:
+        if cell.ctype == xlrd.XL_CELL_EMPTY:
+            return int(0)
+        elif cell.ctype == xlrd.XL_CELL_TEXT or cell.ctype == xlrd.XL_CELL_NUMBER:
             return int(cell.value)
         elif cell.ctype == xlrd.XL_CELL_DATE:
             dt = xlrd.xldate.xldate_as_datetime(cell.value, datemode)
             return int(time.mktime(dt.timetuple()))
         elif cell.ctype == xlrd.XL_CELL_BOOLEAN:
             return 1 if cell.value else 0
-        return 0
+        else:
+            raise Exception("Error: invalid cell type. type:{}".format(cell.ctype))
 
     def _getCellFloat(self, cell):
-        if cell.ctype == xlrd.XL_CELL_TEXT:
+        if cell.ctype == xlrd.XL_CELL_EMPTY:
+            return float(0.0)
+        elif cell.ctype == xlrd.XL_CELL_TEXT:
             return float(cell.value)
         elif cell.ctype == xlrd.XL_CELL_NUMBER:
-            return float((u"%.2f" % cell.value).rstrip('0').rstrip('.'))
+            return float(("%.2f" % cell.value).rstrip('0').rstrip('.'))
         elif cell.ctype == xlrd.XL_CELL_DATE:
             dt = xlrd.xldate.xldate_as_datetime(cell.value, datemode)
-            return u"%.2f" % time.mktime(dt.timetuple())
+            return "%.2f" % time.mktime(dt.timetuple())
         elif cell.ctype == xlrd.XL_CELL_BOOLEAN:
             return  1 if cell.value else 0
-        return 0.0
+        else:
+            raise Exception("Error: invalid cell type. type:{}".format(cell.ctype))
 
     def _getCellBool(self, cell):
-        text = cell.value.lower()
-        if text in [u"", u"nil", u"0", u"false", u"no", u"none", u"否", u"无"]:
-            return u"false"
-        return u"true"
+        if cell.ctype == xlrd.XL_CELL_EMPTY:
+            return False
+        elif cell.ctype == xlrd.XL_CELL_TEXT:
+            value = cell.value.lower()
+            if value in BOOL_NO:
+                return False
+            elif value in BOOL_YES: 
+                return True
+            else:
+                raise Exception("Error: can't switch the value to bool. value:{}".format(value))
+        elif cell.ctype == xlrd.XL_CELL_NUMBER:
+            return float(cell.value) != 0
+        elif cell.ctype == xlrd.XL_CELL_BOOLEAN:
+            return cell.value
+        else:
+            raise Exception("Error: invalid cell type. type:{}".format(cell.ctype))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("excel to lua converter")
-    parser.add_argument(u"-i", u"--input_dir", dest=u"input_dir", help=u"xls dirname", default=u"../xls")
-    parser.add_argument(u"-o", u"--output_dir", dest=u"output_dir", help=u"outpute dirname", default=u"../output/lua")
-    parser.add_argument(u"-m", u"--map_file", dest=u"maps", help=u"xls to struct map", default=u"../config.conf")
+    parser.add_argument("-i", "--input_dir", dest="input_dir", help="excel表文件目录", default="../xls")
+    parser.add_argument("-o", "--output_dir", dest="output_dir", help="输出目录", default="../output")
+    parser.add_argument("-m", "--map_file", dest="maps", help="excel表与输出文件之间映射", default="../config.conf")
+    parser.add_argument("-t", "--type", dest="type", help="导出类型", default="lua")
     args = parser.parse_args()
     converter = Converter(args)
     converter.convertAll()
